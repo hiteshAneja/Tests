008c72921fdaddc4c891848486c066b4d391b666
HItesh Aneja <aneja_hitesh@yahoo.com>
2015-04-12 20:24:11 -0400

    MergeSort with Insertion sort for Optimization.

--- Sorting/MergeSort.cs
+++ Sorting/MergeSort.cs
@@ -7,6 +7,23 @@ namespace Sorting
 {
     public class MergeSort : SortingBase
     {
+        private void InsertionSort(int[] array)
+        {
+            for (int i = 1; i < array.Length; i++) {
+                for (int j = i; j > 0; j--) 
+                {
+                    if (array[j - 1] > array[j])
+                    {
+                        Swap(array, j - 1, j);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+
         public void Merge(int[] array, int[] left, int[] right)
         {
             int j = 0, k = 0;
@@ -25,7 +42,6 @@ namespace Sorting
                 else array[i] = right[k++];
             }
         }
-
 
         public void Sort(int[] array)
         {
@@ -50,6 +66,52 @@ namespace Sorting
 
                 // Now recursively split the right array into two halves until left and right array have only 1 element.
                 Sort(right);
+
+                // Merge and sort the broken arrays.
+                Merge(array, left, right);
+            }
+        }
+
+        /// <summary>
+        /// There is always a cost involve in recursion and merge sort 
+        /// does not prove to be faster when it comes to working with small arrays,
+        /// so for item less than ~4, we here by use insertion sort.
+        /// </summary>
+        public void OptimumSort(int[] array)
+        {
+            if (array.Length > 1)
+            {
+                // Split the left array into two halves.
+                int mid = array.Length / 2;
+
+                // Create left array.
+                int[] left = new int[mid];
+                for (int i = 0; i < left.Length; i++)
+                {
+                    left[i] = array[i];
+                }
+
+                // Create right array.
+                int[] right = new int[array.Length - mid];
+                for (int i = 0; i < right.Length; i++)
+                {
+                    right[i] = array[i + mid];
+                }
+                
+                // For smaller arrays than 4, use insertion sort.
+                if (mid < 4)
+                {
+                    InsertionSort(left);
+                    InsertionSort(right);
+                }
+                else
+                {
+                    // Now recursively split the left array into two halves until left and right array have only 1 element.
+                    Sort(left);
+
+                    // Now recursively split the right array into two halves until left and right array have only 1 element.
+                    Sort(right);
+                }
 
                 // Merge and sort the broken arrays.
                 Merge(array, left, right);
