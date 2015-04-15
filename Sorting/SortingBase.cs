4a51d9d29fb0bcdb3cfa41c4fd7d001aebe412f3
HItesh Aneja <aneja_hitesh@yahoo.com>
2015-04-14 21:45:54 -0400

    Generic SortingBase
    Made sorting base generic to work with different types of data.

--- Sorting/SortingBase.cs
+++ Sorting/SortingBase.cs
@@ -6,22 +6,29 @@ using System.Web;
 
 namespace Sorting
 {
-    public class SortingBase
+    // Added Type constraint to let methods use Icomparable methods.
+    public class SortingBase<T> where T : IComparable<T>
     {
-        public void Display(int[] array)
+        public void Display(T[] array)
         {
             for (int i = 0; i < array.Length; i++)
-			{
+            {
                 Debug.WriteLine(array[i]);
                 Console.WriteLine(array[i]);
-			}
+            }
+
         }
 
-        public void Swap(int[] array, int i, int j)
+        public void Swap(T[] array, int i, int j)
         {
-            int temp = array[i];
+            T temp = array[i];
             array[i] = array[j];
             array[j] = temp;
+        }
+
+        public bool LessOrEqual(T left, T right)
+        {
+            return left.CompareTo(right) <= 0;
         }
     }
 }
