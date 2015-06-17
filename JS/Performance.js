1de9c78219bd15157c3d9b3690fd4f2a3317a000
HItesh Aneja <aneja_hitesh@yahoo.com>
2015-06-17 16:36:38 -0400

    Added new functions
    Added new functions to calculate duration and find difference between two marks.

--- JS/Performance.js
+++ JS/Performance.js
@@ -1,13 +1,14 @@
 (function () {
-    var START = "Start",
+    var self = this,
+        START = "Start",
         END = "End";
 
     var getEntries = function (name) {
         var result;
         if (name) {
-            result = performance.getEntriesByName(name);
+            result = self.getEntriesByName(name);
         } else {
-            result = performance.getEntriesByType('measure');
+            result = self.getEntriesByType('measure');
         }
         return result;
     };
@@ -17,46 +18,66 @@
 
     // Starts the mark and clears the previous mark with the same name.
     this.startMark = function (name) {
-        performance.clearMarks(name + START, name + END);
-        performance.mark(name + START);
+        if (self.mark && name) {
+            self.clearMarks(name + START, name + END);
+            self.mark(name + START);
+        }
     };
 
     // Ends the mark and calculate the measure between this and the previous mark with the same name.
     this.endMark = function (name) {
-        performance.mark(name + END);
-        performance.clearMeasures(name);
-        performance.measure(name, (name + START), (name + END));
-    };
-
-    // Prints all the measures in the console.
-    this.printMeasures = function (name) {
-        var result = getEntries(name);
-
-        if (result && result.length > 0) {
-            result.forEach(function (item, index) { 
-                console.log(item.name + ":  " + Math.round(item.duration * 100) / 100 + "ms") 
-            });
+        if (self.mark && name) {
+            self.mark(name + END);
+            self.clearMeasures(name);
+            self.measure(name, (name + START), (name + END));
         }
     };
     
     // Gets all the measures in an easy to use JSON object { name, duration }.
     this.getMeasures = function (name) {
-        var response = [],
-            result = getEntries(name);
+        if (self.mark) {
+            var response = [],
+                result = getEntries(name);
 
-        if (result && result.length > 0) {
-            result.forEach(function (item, index) { 
-                response.push({ 
-                    name: item.name,
-                    duration: (Math.round(item.duration * 100) / 100)
+            if (result && result.length > 0) {
+                result.forEach(function (item) {
+                    response.push({
+                        name: item.name,
+                        duration: (Math.round(item.duration * 100) / 100)
+                    });
                 });
-            });
+            }
+
+            return response;
         }
-        
-        return response;
+        return {};
     };
 
-}.call(window.performance));
+    // Prints all the measures on the console.
+    this.printMeasures = function (name) {
+        return JSON.stringify(this.getMeasures(name));
+    };
+
+    // Get duration of one particular measure.
+    this.duration = function (name) {
+        if (self.mark && name) {
+            var entries = this.getMeasures(name);
+            if (entries && entries.length > 0) {
+                return entries[0].duration;
+            }
+        }
+    };
+
+    // Measures the difference between two marks, store the measure and return the result.
+    this.difference = function (name, from, to) {
+        if (self.mark && from && to) {
+            self.measure(name, from, to);
+            return this.getMeasures(name);
+        }
+    };
+
+
+}.apply(window.performance));
 
 
 /*
